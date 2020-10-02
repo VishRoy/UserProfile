469c081ff00092c6608eeae2e0810a304d00c504
Vishakha <vishakha.roy2020@gmail.com>
2020-10-02 16:29:01 +0530

    Updates

---
+++ precache-manifest.bac29fc1b91481496f14c0a312530922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae72ef89190dc8a62fe51afeb6fbc1c",
+    "url": "/UserProfile/index.html"
+  },
+  {
+    "revision": "5fc37b836d3930291626",
+    "url": "/UserProfile/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "54f1246421ba0faa19df",
+    "url": "/UserProfile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fc37b836d3930291626",
+    "url": "/UserProfile/static/js/2.b112096e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/UserProfile/static/js/2.b112096e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f1246421ba0faa19df",
+    "url": "/UserProfile/static/js/main.19d49e8d.chunk.js"
+  },
+  {
+    "revision": "eb608586482474040daa",
+    "url": "/UserProfile/static/js/runtime-main.06d59de5.js"
+  }
+]);
