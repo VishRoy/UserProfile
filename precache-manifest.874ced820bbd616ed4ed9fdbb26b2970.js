423d518f5fc69b24742a0b893cda1a143d1b28fc
Vishakha <vishakha.roy2020@gmail.com>
2020-10-02 16:53:59 +0530

    Updates

---
+++ precache-manifest.874ced820bbd616ed4ed9fdbb26b2970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f926b7dfb63b3c66e2656379854409f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc37b836d3930291626",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7de3d11aa0ebaa211640",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fc37b836d3930291626",
+    "url": "/static/js/2.b112096e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b112096e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de3d11aa0ebaa211640",
+    "url": "/static/js/main.d841f0f3.chunk.js"
+  },
+  {
+    "revision": "32891900afb91d2eb053",
+    "url": "/static/js/runtime-main.265c1206.js"
+  }
+]);
